bbc9d47f10e4c06a36d0a040837e5f200756e376
ozgeh <ozgeh@DESKTOP-3UNLGM2>
2022-03-21 21:21:59 +0300

    Proje dosyası ekle.

---
+++ WpfApp_NTP_ders/Data/SQLkod.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE VT2022
+GO
+USE VT2022
+GO
+CREATE TABLE [dbo].[Musteri]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ad] VARCHAR(50) NULL, 
+    [soyad] VARCHAR(50) NULL, 
+    [tel] NCHAR(11) NULL, 
+    [mail] VARCHAR(50) NULL, 
+    [adres] VARCHAR(MAX) NULL
+)
+
+CREATE TABLE [dbo].[Hizmet]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [hizmetad] VARCHAR(50) NULL, 
+    [fiyat] INT NULL
+)
---
+++ WpfApp_NTP_ders/Views/HizmetlerPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_NTP_Sinav1
+{
+    /// <summary>
+    /// OgrencilerPage.xaml etkileşim mantığı
+    /// </summary>
+    public partial class HizmetlerPage : Page
+    {   
+        public HizmetlerPage()
+        {
+            InitializeComponent();
+           
+        }
+    }
+}
